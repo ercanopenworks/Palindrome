ae27fc58d7c48b8a4fb01d0db293cf4fc20d8573
Ercan-home <ercanerdogan@gmail.com>
2019-10-14 03:08:00 +0300

    Check palindrom text

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static bool IsAlmostPolindrome(string pString)
+        {
+            string rev;
+            char[] ch = pString.ToCharArray();
+
+            Array.Reverse(ch);
+            rev = new string(ch);
+
+            bool b = pString.Equals(rev, StringComparison.OrdinalIgnoreCase);
+
+            return b;
+            
+        }
+        static void Main(string[] args)
+        {
+            string string1;
+            string1 = "abccba";
+
+            if (IsAlmostPolindrome(string1))
+            {
+                Console.WriteLine("" + string1 + " is a Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine(" " + string1 + " is not a Palindrome!");
+            }
+            Console.Read();
+
+
+        }
+    }
+}
